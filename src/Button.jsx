70ae392ca644ed9ecdf8bdda4dad34354fb9d635
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-01-18 22:52:52 +0200

    2

---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = (props) => {
+    console.log(props)
+    return (
+        <button className={classNames('button', {
+            'button--outline': props.outl
+        })}>
+            {props.children}
+        </button>
+    );
+};
+
+export default Button;
