7bdf76d0a62edf45522c293b6d6e1cb35d462502
maxbendas <maxbendas86@gmail.com>
2021-01-19 16:00:04 +0200

    3.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import Header from "./Header";
-import Button from "./Button";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
-            <Button outl>11111111111</Button>
-            <Button>22222222222</Button>
             <div className="content">
                 <div className="container">
                     <div className="content__top">
-                        <div className="categories">
-                            <ul>
-                                <li className="active">Все</li>
-                                <li>Мясные</li>
-                                <li>Вегетарианская</li>
-                                <li>Гриль</li>
-                                <li>Острые</li>
-                                <li>Закрытые</li>
-                            </ul>
-                        </div>
+                        <Categories
+                            onClickItem={(name)=>console.log(name)}
+                            items={['Мясные', 'Вегетарианская',
+                                'Гриль', 'Острые', 'Закрытые']}/>
                         <div className="sort">
                             <div className="sort__label">
                                 <svg
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = ({onclick, children, className, outline}) => {
+    return (
+        <button className={classNames('button', className, {
+            'button--outline': outline
+        })}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
